02caff1550c72f92457d8e0b0b8ba158f3fd2d0c
Isha van Baar <isha.vanbaar@gmail.com>
2015-02-28 22:35:28 +0100

    button changes clock

--- js/ArcTweenClock.js
+++ js/ArcTweenClock.js
@@ -1,11 +1,12 @@
-
 var w = 960,
     h = 500,
-hourScale = 0;
+    hourScale = 0, //0 if 12h, 1 if 24h, 2 if 1 week
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
 
 var fields = [
-  {id: 0, name: "hours", value: 0, size: 12, innerRadius: 100, outerRadius: 120},
-  {id: 1, name: "minutes", value: 0, size: 60, innerRadius: 70, outerRadius: 90},
+  {name: "hours", value: 0, size: 12, innerRadius: 100, outerRadius: 120},
+  {name: "minutes", value: 0, size: 60, innerRadius: 70, outerRadius: 90},
   {name: "seconds", value: 0, size: 60, innerRadius: 40, outerRadius: 60}
 ];
 
@@ -21,10 +22,11 @@ var svg2 = d3.select("#clock").append("svg:svg")
   .append("svg:g")
     .attr("transform", "translate(0," + (h / 2) + ")");
 
-setInterval(function() {
+setInterval(function(d) {
   var now = new Date();
   fields[0].previous = fields[0].value; 
-  fields[0].value = timeForHours(now.getHours());
+
+  fields[0].value = timeForHourPath(now.getHours(), now.getDay());
   fields[1].previous = fields[1].value; fields[1].value = now.getMinutes();
   fields[2].previous = fields[2].value; fields[2].value = now.getSeconds();
 
@@ -33,8 +35,9 @@ setInterval(function() {
 
   path.enter().append("svg:path")
     .attr("transform", function(d, i) { return "translate(" + w/2 + ",0)"; })
-    .attr("id", "circular-path")
-    .transition()
+    .attr("id", "circular-path");
+  
+  path.transition()
       .ease("elastic")
       .duration(750)
       .attrTween("d", arcTween);
@@ -49,6 +52,8 @@ setInterval(function() {
       .duration(750)
       .attrTween("d", arcTween)
       .remove();
+  
+  $("#time-button").text(timeStringForButton(now.getUTCDay(), now.getHours(), now.getMinutes()));
 }, 1000);
 
 function arcTween(b) {
@@ -58,13 +63,51 @@ function arcTween(b) {
   };
 }
 
-function timeForHours(hours) {
+function timeForHourPath(hours, day) {
     if (hourScale === 0) {  
         if (hours > 11) {
             hours = hours - 12;
         }
-    } else if (hourScale === 2) {
-        //TODO
+        fields[0].size = 12;
+    }  
+    else if (hourScale === 1) {
+        fields[0].size = 24;
+    }
+    else if (hourScale === 2) {
+        fields[0].size = 168;
+        hours = hours + (day-1) * 24;
     }
     return hours;
+}
+
+function timeStringForButton(day, hours, minutes) {
+    var timeString = '';
+    if (hourScale === 0) {  
+        if (hours > 0 && hours < 6) {
+            timeString = 'Night';
+        } else if (hours > 6 && hours < 12) {
+            timeString = 'Morning';
+        } else if (hours > 12 && hours < 18) {
+            timeString = 'Midday';
+        } else {
+            timeString = 'Evening';
+        }
+    } 
+    else if (hourScale === 1) {
+        var minuteString = minutes + '';
+        if (minutes < 10) { minuteString = '0' + minutes}
+        timeString = hours + ":" + minutes;    
+    }
+    else if (hourScale === 2) {
+        timeString = days[day];    
+    }
+    return timeString;
+}
+
+function switchTime() {
+    if (hourScale === 2) {
+        hourScale = 0;
+    } else {
+        hourScale++;
+    }
 }
